5182c9ffcd4bd4ef223ac34f2935db1bb5ff2312
oscaroceguera <oscaroceguera@gmail.com>
2017-03-01 23:34:59 -0700

    GENERIC CIRCULAR PROGRESS

--- app/components/index.js
+++ app/components/index.js
@@ -7,6 +7,7 @@ export GenericTextField from './commons/GenericTextField'
 export GenericCheckbox from './commons/GenericCheckbox'
 export GenericTextFieldForm from './commons/GenericTextFieldForm'
 export GenericAutoComplete from './commons/GenericAutoComplete'
+export GenericCircularProgress from './commons/GenericCircularProgress'
 // Solicitud de servicio
 export IsClientForm from './SolicitudServicio/IsClientForm'
 export SolicitudGeneralesEmpresa from './SolicitudServicio/SolicitudGeneralesEmpresa'
