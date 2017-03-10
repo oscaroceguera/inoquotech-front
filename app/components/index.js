df4ea471afa17565d2f8d6ec491e2356082def8a
oscaroceguera <oscaroceguera@gmail.com>
2017-03-10 01:06:07 -0700

    UI & SAGAS ::: MODULE LIST

--- app/components/index.js
+++ app/components/index.js
@@ -1,6 +1,5 @@
 export Home from './Home/Home'
 export TopBar from './TopBar/TopBar'
-// Commons
 export FormRequestWrapper from './commons/FormRequestWrapper'
 export FormRadiobuttomWrapper from './commons/FormRadiobuttomWrapper'
 export GenericRadioButton from './commons/GenericRadioButton'
@@ -9,7 +8,7 @@ export GenericCheckbox from './commons/GenericCheckbox'
 export GenericTextFieldForm from './commons/GenericTextFieldForm'
 export GenericAutoComplete from './commons/GenericAutoComplete'
 export GenericCircularProgress from './commons/GenericCircularProgress'
-// Solicitud de servicio
 export IsClientForm from './SolicitudServicio/IsClientForm'
 export SolicitudGeneralesEmpresa from './SolicitudServicio/SolicitudGeneralesEmpresa'
 export SolicitudServicioRequerido from './SolicitudServicio/SolicitudServicioRequerido'
+export ModuleItem from './ModuleItem'
--- app/containers/ModulesList/ModulesListContainer.js
+++ app/containers/ModulesList/ModulesListContainer.js
@@ -1,25 +1,51 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import {catalogsActions} from 'reducers/catalogs'
+import _ from 'lodash'
+import { catalogsActions } from 'reducers/catalogs'
+import styled from 'styled-components'
+import { ModuleItem } from 'components'
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1.5em 3em;
+  margin: 0 auto;
+  max-width: 1000px;
+`
 
 class ModulesListContainer extends Component {
+  static propTypes = {
+    moduleTypes: PropTypes.array.isRequired
+  }
   componentWillMount () {
     this.props.catForDashboardRequest()
   }
   render () {
+    const { moduleTypes } = this.props
+    const modulesOrder = _.orderBy(moduleTypes, 'value', 'asc')
     return (
-      <h1>ModulesListContainer</h1>
+      <ListContainer>
+        {
+          modulesOrder.map(({value, iconType, description, path}, index) => (
+            <ModuleItem
+              key={index}
+              value={value}
+              iconType={iconType}
+              description={description}
+              path={path}
+            />
+          ))
+        }
+      </ListContainer>
     )
   }
 }
 
-// TODO: PROPTYPES
-const mapStateToProps = (state, props) => {
-  console.log('STATE', state);
-  console.log('PROPS', props);
+const mapStateToProps = (state) => {
+  const catalogsJS = state.catalogs.toJS()
   return {
-    saludo: 'hola'
+    moduleTypes: catalogsJS.modules
   }
 }
 
