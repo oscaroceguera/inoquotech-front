aff0675807b80baf8e9544cf2060ff7efc5c30ef
oscaroceguera <oscaroceguera@gmail.com>
2017-03-12 13:17:43 -0700

    BTN ::: ADD USER IN USERS LIST

--- app/components/commons/GenericTable.js
+++ app/components/commons/GenericTable.js
@@ -5,39 +5,89 @@ import {
   TableHeader,
   TableHeaderColumn,
   TableRow,
-  TableRowColumn
+  TableRowColumn,
+  FloatingActionButton
 } from 'material-ui'
+import * as Colors from 'material-ui/styles/colors'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import styled from 'styled-components'
 
+const TableContainer = styled.div`
+  position: relative;
+`
+
+const BtnContainer = styled.div`
+  position: fixed;
+  right: 1em;
+  bottom: 2em;
+`
 
 // TODO: PropTypes
 const GenericTable = (props) => {
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHeaderColumn>NOMBRE</TableHeaderColumn>
-          <TableHeaderColumn>EMAIL</TableHeaderColumn>
-          <TableHeaderColumn>STATUS</TableHeaderColumn>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
-          <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
-          <TableRowColumn>Activo</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
-          <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
-          <TableRowColumn>Activo</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
-          <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
-          <TableRowColumn>Activo</TableRowColumn>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <TableContainer>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>NOMBRE</TableHeaderColumn>
+            <TableHeaderColumn>EMAIL</TableHeaderColumn>
+            <TableHeaderColumn>STATUS</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>Oscar Eduardo Oceguera Bibriesca</TableRowColumn>
+            <TableRowColumn>oscaroceguera@gmail.com</TableRowColumn>
+            <TableRowColumn>Activo</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <BtnContainer>
+        <FloatingActionButton backgroundColor={Colors.red500}>
+          <ContentAdd />
+        </FloatingActionButton>
+      </BtnContainer>
+    </TableContainer>
   )
 }
 
